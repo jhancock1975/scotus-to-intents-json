a9aadb6860dc6dd348d28929f54aacf9ec02da72
John Hancock <jhancock1975@gmail.com>
2017-09-15 10:31:45 -0400

    ignore eclipse project files, initial commit of converter

---
+++ src/main/java/converter/ScotusToIntent.java
@@ -0,0 +1,23 @@
+package main.java.converter;
+
+public class ScotusToIntent {
+	public static void main(String[] args) {
+		ScotusToIntent  sti = new ScotusToIntent();
+		if (sti.validate(args)) {
+			
+		} else {
+			sti.printUsage();
+		}
+
+	}
+
+	private void printUsage() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private boolean validate(String[] args) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
